f65519719a1a14fc2309606a232ca0426c4d79d8
erenkaraboga <darkbull1073@gmail.com>
2022-07-17 11:09:23 +0300

    UserModel Updated

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,15 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
-  final String name;
-  final double lat;
-  final double long;
-
-  UserModel({
-    required this.name,
-    required this.lat,
-    required this.long,
-  });
+   String? name;
+   double? lat;
+   double? long;
+  UserModel(){}
 
   Map<String, dynamic> toMap() {
     return {
