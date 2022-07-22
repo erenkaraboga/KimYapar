bc1e14f7593e878bea97d2e1012c861d8de3baaf
erenkaraboga <darkbull1073@gmail.com>
2022-07-22 12:43:54 +0300

    User Model Updated

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,21 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
-   String? name;
-   double? lat;
-   double? long;
-  UserModel(){}
+  int? id;
+  String? imageUrl;
+  String? name;
+  double? lat;
+  double? long;
+  UserModel() {}
 
   Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'lat': lat,
-      'long': long,
-    };
+    return {'name': name, 'lat': lat, 'long': long, 'imageUrl': imageUrl,'id':id};
   }
 
   UserModel.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
       : name = doc.data()!["name"],
         lat = doc.data()!["lat"],
-        long = doc.data()!["long"];
+        long = doc.data()!["long"],
+        imageUrl = doc.data()!["imageUrl"],
+        id = doc.data()!["id"];
 }
