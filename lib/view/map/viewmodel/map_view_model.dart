dacbc83dc4888e8c92c401eab9cf77b5c22186f3
erenkaraboga <darkbull1073@gmail.com>
2022-07-24 19:54:47 +0300

    Use Mobx State Management

---
+++ lib/view/map/viewmodel/map_view_model.dart
@@ -0,0 +1,30 @@
+import 'dart:developer';
+
+import 'package:kimyapar/view/map/model/UserModel.dart';
+import 'package:kimyapar/view/map/service/IMapService.dart';
+import 'package:mobx/mobx.dart';
+part 'map_view_model.g.dart';
+
+class MapViewModel = _MapViewModelBase with _$MapViewModel;
+
+abstract class _MapViewModelBase with Store {
+  final IMapService mapService;
+  @observable
+  List<UserModel> list = [];
+  _MapViewModelBase(this.mapService);
+  @observable
+  bool isLoading = false;
+  @action
+  Future<void> fetcAllMaps() async {
+    changeLoading();
+    final response = await mapService.filterGeo();
+    changeLoading();
+    list = response;
+    inspect(list);
+  }
+
+  @action
+  void changeLoading() {
+    isLoading = !isLoading;
+  }
+}
