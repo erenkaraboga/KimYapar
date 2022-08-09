047584f1b72226af56003966cad2274cf80d454c
erenkaraboga <darkbull1073@gmail.com>
2022-08-09 13:46:49 +0300

    LoginWidgets Updated

--- lib/product/widgets/login/loginWidgets.dart
+++ lib/product/widgets/login/loginWidgets.dart
@@ -1,61 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
-import 'package:form_field_validator/form_field_validator.dart';
 import 'package:kimyapar/core/constants/path.dart';
 import 'package:kimyapar/product/utilities/lottie.dart';
-import 'package:kimyapar/product/widgets/login/swipeButton.dart';
-import 'package:kimyapar/view/map/view/mapPage.dart';
-import 'package:slidable_button/slidable_button.dart';
+import 'package:kimyapar/product/widgets/login/signIn/swipeButton.dart';
+import 'package:kimyapar/product/widgets/login/signUp/swipeButton.dart';
 
 import '../../../core/base/text.dart';
 import '../../../core/constants/styles.dart';
 import '../../../core/languages/tr.dart';
 
 class LoginWidgets {
-  static LoginButton(GlobalKey<FormState> formKey, BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (formKey.currentState!.validate()) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const MapSelect()),
-          );
-        }
-      },
-      child: Container(
-        alignment: Alignment.center,
-        width: 250,
-        decoration: ContainerStyles.LoginBox,
-        child: const Padding(
-            padding: EdgeInsets.all(12.0),
-            child: CreateText(
-                text: Tr.signin, style: TextStyles.loginButtonStyle)),
-      ),
-    );
-  }
-
-  static PassField(MultiValidator passwordValidator) {
-    return SizedBox(
-      width: 260,
-      height: 60,
-      child: TextFormField(
-        obscureText: true,
-        validator: passwordValidator,
-        decoration: TextFieldStyles.PassField,
-      ),
-    );
-  }
-
-  static MailField(MultiValidator emailValidator) {
-    return SizedBox(
-        width: 260,
-        height: 60,
-        child: TextFormField(
-          validator: emailValidator,
-          decoration: TextFieldStyles.MailField,
-        ));
-  }
-
   static ForgotPass() {
     return Padding(
       padding: const EdgeInsets.fromLTRB(20, 0, 30, 0),
@@ -74,8 +28,12 @@ class LoginWidgets {
     );
   }
 
-  static SwippableBtn() {
-    return const SwippableButton(position: SlidableButtonPosition.start);
+  static SwippableBtnUP() {
+    return const SwippableButtonUP();
+  }
+
+  static SwippableBtnIN() {
+    return const SwippableButtonIN();
   }
 
   static PleaseSignText() {
@@ -104,11 +62,19 @@ class LoginWidgets {
     );
   }
 
-  static Lottiee() {
+  static LottieLogin() {
     return const SizedBox(
       height: 175,
       width: 175,
       child: LottieProgress(path: AppPaths.login),
+    );
+  }
+
+  static LottieLoginSuccess() {
+    return const SizedBox(
+      height: 175,
+      width: 175,
+      child: LottieProgress(path: AppPaths.loginsuccess),
     );
   }
 }
