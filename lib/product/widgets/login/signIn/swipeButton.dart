77d1553f3ad4fd372c7c559b5c8cefa1b6c1ad81
erenkaraboga <darkbull1073@gmail.com>
2022-08-09 13:46:31 +0300

    LoginSwipeButtons Added

---
+++ lib/product/widgets/login/signIn/swipeButton.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kimyapar/product/widgets/login/signUp/swipeButton.dart';
+import 'package:kimyapar/view/login/view/SignUp.dart';
+import 'package:slidable_button/slidable_button.dart';
+
+class SwippableButtonIN extends StatefulWidget {
+  const SwippableButtonIN({Key? key,}) : super(key: key);
+ 
+  @override
+  State<SwippableButtonIN> createState() => SwippableButtonINState();
+}
+
+class SwippableButtonINState extends State<SwippableButtonIN> {
+  @override
+  Widget build(BuildContext context) {
+    return HorizontalSlidableButton(
+      width: MediaQuery.of(context).size.width / 2,
+      initialPosition:SlidableButtonPosition.start ,
+      buttonWidth: 90.0,
+      color: Colors.black12,
+      buttonColor: Colors.orange.shade200,
+      label: const Center(child: Text('Giriş')),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text('Left'),
+            Text('Kayıt Ol'),
+          ],
+        ),
+      ),
+      onChanged: (position) {
+        if (position == SlidableButtonPosition.end) {
+          Get.toNamed("/signUp");
+        } else {
+          return;
+        }
+      },
+    );
+  }
+}
---
+++ lib/product/widgets/login/signUp/swipeButton.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:slidable_button/slidable_button.dart';
+
+class SwippableButtonUP extends StatefulWidget {
+  const SwippableButtonUP({Key? key}) : super(key: key);
+  @override
+  State<SwippableButtonUP> createState() => _SwippableButtonUPState();
+}
+
+class _SwippableButtonUPState extends State<SwippableButtonUP> {
+  @override
+  Widget build(BuildContext context) {
+    return HorizontalSlidableButton(
+      width: MediaQuery.of(context).size.width / 2,
+      initialPosition: SlidableButtonPosition.end,
+      buttonWidth: 90.0,
+      color: Colors.black12,
+      buttonColor: Colors.orange.shade200,
+      label: const Center(child: Text('Kayıt Ol')),
+      dismissible: false,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text('Giriş'),
+            
+          ],
+        ),
+      ),
+      onChanged: (position) {
+        if (position == SlidableButtonPosition.start) {
+          Get.toNamed("/signIn");
+        } else {
+          return;
+        }
+      },
+    );
+  }
+}
