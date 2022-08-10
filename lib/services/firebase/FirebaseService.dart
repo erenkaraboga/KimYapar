f67d3e244ea4467984870f6525cc6b52b3797268
erenkaraboga <darkbull1073@gmail.com>
2022-08-10 14:06:24 +0300

    Firebase Service Seperated

--- lib/services/firebase/FirebaseService.dart
+++ lib/services/firebase/FirebaseService.dart
@@ -1,16 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:kimyapar/services/firebase/IFirebaseService.dart';
 
 import '../../view/map/model/UserModel.dart';
 
 class FirebaseService extends IFirebaseService{
+  FirebaseService(super.auth, super.db);
   @override
-  Future<List<UserModel>> getAllUsers() {
-    
+  Future<List<UserModel>> getAllUsers()async {
+     QuerySnapshot<Map<String, dynamic>> snapshot =
+        await super.db.collection("users").get();
+    return snapshot.docs
+        .map((docSnapshot) => UserModel.fromDocumentSnapshot(docSnapshot))
+        .toList();
   }
-
   @override
-  void getCurrentUser() {
-    // TODO: implement getCurrentUser
+  void getCurrentUser(String id) {
+    super.db
+    .collection('users')
+    .doc(id)
+    .get()
+    .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        print('Document data: ${documentSnapshot.data()}');
+      } else {
+        print('Document does not exist on the database');
+      }
+    });
   }
-
 }
--- lib/services/firebase/IFirebaseService.dart
+++ lib/services/firebase/IFirebaseService.dart
@@ -1,7 +1,13 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import '../../view/map/model/UserModel.dart';
 abstract class IFirebaseService{
+   final FirebaseAuth auth;
+  final FirebaseFirestore db;
+  IFirebaseService(this.auth, this.db);
   Future<List<UserModel>> getAllUsers();
-  void getCurrentUser();
+  void getCurrentUser(String id);
 
 }
