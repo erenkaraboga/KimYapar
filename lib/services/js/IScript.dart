0f84e20608ef0fd49bb0943476703d92cbd3df80
erenkaraboga <darkbull1073@gmail.com>
2022-08-30 20:34:54 +0300

    Script Class Added

---
+++ lib/services/js/IScript.dart
@@ -0,0 +1,6 @@
+abstract class IScript{
+  void add();
+  void delete();
+  void update();
+  void getAll();
+}
---
+++ lib/services/js/script.dart
@@ -0,0 +1,24 @@
+import 'package:kimyapar/services/js/IScript.dart';
+
+class Script extends IScript{
+  @override
+  void add() {
+    // TODO: implement add
+  }
+
+  @override
+  void delete() {
+    // TODO: implement delete
+  }
+
+  @override
+  void getAll() {
+    // TODO: implement getAll
+  }
+
+  @override
+  void update() {
+    // TODO: implement update
+  }
+  
+}
